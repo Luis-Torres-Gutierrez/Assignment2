6dd011cbcac30679d79dea9b31ee402de661c285
Luis Torres <ltorresg@asu.edu>
2019-10-07 20:07:03 -0700

    Created the initial version.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	/**
+	 * @author Luis Torres
+	 * ltorresg
+	 * Assignment 2
+	 */
+
+	private int total;
+	
+	/**
+	 * Creates a new instance of the AddingMachine class.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the current total value.
+	 * 
+	 * @return integer representation of the total value
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the total.
+	 * 
+	 * @param value integer value to be added to the total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value from the total.
+	 * 
+	 * @param value integer value to be subtracted from the total.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string of the transactional history.
+	 * 
+	 * @return string representation of the summary of the transactions performed.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
